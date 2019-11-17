eec8eaf6a52a3a417ab4167bbb6f9169fd05c1eb
dhileepan <ddhileepan1989@gmail.com>
2019-11-17 20:39:17 +0100

    JUNIT code refractoring

--- src/test/java/com/kata/test/FizzBuzzTest.java
+++ src/test/java/com/kata/test/FizzBuzzTest.java
@@ -15,40 +15,45 @@ public class FizzBuzzTest {
 		fizzBuzz = new FizzBuzz();
 	}
 
+	/**
+	 * This method should test whether given number is returned if it is not
+	 * multiples of 3 and 5 or not
+	 */
 	@Test
-	public void shouldReturn1For1() {
-
-		assertFinal(1,"1");
+	public void shouldReturnSameNumberForNonMultiplesOf3And5() {
+		assertFinal(1, "1");
+		assertFinal(2, "2");
+		assertFinal(4, "4");
 	}
-	
+
 	@Test
 	public void shouldReturnFizzForMultiplesOf3() {
 
-		assertFinal(3,"Fizz");
-		assertFinal(6,"Fizz");
-		assertFinal(9,"Fizz");
-		assertFinal(12,"Fizz");
+		assertFinal(3, "Fizz");
+		assertFinal(6, "Fizz");
+		assertFinal(9, "Fizz");
+		assertFinal(12, "Fizz");
 	}
-	
+
 	@Test
 	public void shouldReturnBuzzForMultiplesOf5() {
 
-		assertFinal(5,"Buzz");
-		assertFinal(10,"Buzz");
-		assertFinal(20,"Buzz");
+		assertFinal(5, "Buzz");
+		assertFinal(10, "Buzz");
+		assertFinal(20, "Buzz");
 	}
+
 	@Test
 	public void shouldReturnFizzBuzzForMultiplesOf3And5() {
 
-		assertFinal(15,"FizzBuzz");
-		assertFinal(30,"FizzBuzz");
-		assertFinal(45,"FizzBuzz");
+		assertFinal(15, "FizzBuzz");
+		assertFinal(30, "FizzBuzz");
+		assertFinal(45, "FizzBuzz");
 	}
-	
-	private void assertFinal(int number,String sExpectedResult)
-	{
-		String sResult=fizzBuzz.check(number);
+
+	private void assertFinal(int number, String sExpectedResult) {
+		String sResult = fizzBuzz.check(number);
 		assertEquals(sExpectedResult, sResult);
-		
+
 	}
 }
